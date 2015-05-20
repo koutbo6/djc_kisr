bd7524cc889347361a5f92431fef0b5c2eeda799
Mohammad AlMarzouq <m.almarzouq@kbsoft.com.kw>
2015-05-20 20:40:11 +0300

    completed poll details

--- kbpollproj/polls/urls.py
+++ kbpollproj/polls/urls.py
@@ -1,13 +1,7 @@
-from django.conf.urls import patterns, url
-# import the views module
-# this was not needed Django pre-1.8
+from django.conf.urls import url
 from polls import views
 
 urlpatterns = [
     url(r'^list/', views.poll_list, name='poll_list',),
+    url(r'^poll/(?P<poll_id>\d+)/$', views.poll_details, name='poll_details',),
 ]
-
-# This is how to do it in Django pre-1.8
-# urlpatterns = patterns('polls.views',
-#     url(r'^list/', 'poll_list', name='poll_list',),
-# )
--- kbpollproj/polls/views.py
+++ kbpollproj/polls/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404
 from .models import Poll
 
 
@@ -6,3 +7,11 @@ def poll_list(request):
     # construct a queryset
     qs = Poll.objects.all()
     return render(request, "poll_list.html", {"polls": qs})
+
+
+def poll_details(request, poll_id):
+    try:
+        poll = Poll.objects.get(pk=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    return render(request, "poll_details.html", {"poll": poll})
